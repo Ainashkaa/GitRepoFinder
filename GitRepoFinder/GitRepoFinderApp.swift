5c95cefaecf2cc11931b28e92a1b7ed5ad5a4a99
Ainash <turbayeva17@gmail.com>
2023-11-05 12:25:54 +0600

    Initial commit

---
+++ GitRepoFinder/GitRepoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepoFinderApp.swift
+//  GitRepoFinder
+//
+//  Created by Ainash Turbayeva on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitRepoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
